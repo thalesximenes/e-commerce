2157b8a65c49a074e21bf2bfa0f8a4732d48b378
ericiannj <ericiannj@gmail.com>
2023-05-26 15:08:29 -0300

    chore: adjust tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Register from './pages/Register/Register'
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,22 +6,42 @@ const Login: React.FC = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="login-screen">
-      <div className="login-form">
-        <div className="login-header">
-          <h1>🛍️ E-commerce</h1>
-        </div>
-        <div className="register-input-container">
-          <input type="email" placeholder="E-mail" className="login-input" />
-          <input type="password" placeholder="Senha" className="login-input" />
-        </div>
-
-        <div className="button-container">
-          <button className="login-button" onClick={() => navigate('/')}>
-            Login
-          </button>
-          <a href="/register">Ainda não possui conta?</a>
-          <a href="/">Voltar para tela inicial</a>
+    <div className="flex justify-center items-center h-screen w-screen bg-green-900">
+      <div className="flex flex-col h-1/2 justify-center items-center bg-white rounded-lg">
+        <div className="flex flex-col  h-3/4 justify-center items-center bg-blue-400">
+          <div className="flex flex-col items-center h-1/2 bg-orange-500">
+            <div className="flex flex-col">
+              <h1 className="text-3xl text-gray-500">🛍️ E-commerce</h1>
+            </div>
+            <div className="flex flex-col w-3/4 mb-4 bg-red-400">
+              <input
+                type="email"
+                placeholder="E-mail"
+                className="p-2 border-2 border-gray-300 rounded-md mb-2"
+              />
+              <input
+                type="password"
+                placeholder="Senha"
+                className="p-2 border-2 border-gray-300 rounded-md"
+              />
+            </div>
+            <div className="w-2/4">
+              <button
+                className="login-button  p-2 bg-red-500 text-white rounded-md"
+                onClick={() => navigate('/')}
+              >
+                Login
+              </button>
+              <div className="">
+                <a href="/register" className="">
+                  Ainda não possui conta?
+                </a>
+                <a href="/" className="">
+                  Voltar para tela inicial
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
